619427134d405a0d7c04bcc75c70924c02945c56
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-01-26 12:49:56 -0300

    feat(separator): expose separator width as variable

--- packages/qr/src/compose-separators/compose-separators.ts
+++ packages/qr/src/compose-separators/compose-separators.ts
@@ -1,7 +1,8 @@
 import { FINDER_PATTERN } from "../compose-finders/compose-finders"
 import type { Matrix } from "../get-matrix/get-matrix"
 
-const SIZE = 8
+const LENGTH = 8
+export const WIDTH = 1
 const VALUE = 0
 
 export function composeSeparators(matrix: Matrix): Matrix {
@@ -9,19 +10,19 @@ export function composeSeparators(matrix: Matrix): Matrix {
   const end = matrix.length
 
   // top-left separator
-  for (let i = 0; i < SIZE; i++) {
+  for (let i = 0; i < LENGTH; i++) {
     matrix[i][offset] = VALUE
     matrix[offset][i] = VALUE
   }
 
   // top-right separator
-  for (let i = 0; i < SIZE; i++) {
+  for (let i = 0; i < LENGTH; i++) {
     matrix[i][end - offset - 1] = VALUE
     matrix[offset][end - i - 1] = VALUE
   }
 
   // bottom-left separator
-  for (let i = 0; i < SIZE; i++) {
+  for (let i = 0; i < LENGTH; i++) {
     matrix[end - i - 1][offset] = VALUE
     matrix[end - offset - 1][i] = VALUE
   }
