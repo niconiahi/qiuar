8bb3e3255477f1803ea42ac35215b9632a1425fc
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-01-27 21:11:43 -0300

    feat(playground): add canvas and renderer function

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,1 +1,35 @@
-export const a = 'si'
+import type { Matrix } from "@qiuar/qr"
+import { QUIET_ZONE_WIDTH, getMatrix } from "@qiuar/qr"
+
+const BITE_WIDTH = 10
+
+function renderQr(): void {
+  const canvas = document.getElementById("myCanvas")
+  if (!canvas) {
+    return
+  }
+  const context = (canvas as HTMLCanvasElement).getContext("2d")
+  if (!context) {
+    return
+  }
+  const input = "Hello World"
+  const matrix = getMatrix(input)
+  drawMatrix(matrix, context)
+}
+
+function drawMatrix(matrix: Matrix, context: CanvasRenderingContext2D): void {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const SEPARATION = QUIET_ZONE_WIDTH / 2
+      context.fillStyle = matrix[i + SEPARATION][j + SEPARATION] === 0 ? "white" : "black"
+      context.fillRect(
+        j * BITE_WIDTH,
+        i * BITE_WIDTH,
+        BITE_WIDTH,
+        BITE_WIDTH,
+      )
+    }
+  }
+}
+
+renderQr()
